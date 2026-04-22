03fe062ea9a0cd0c3605d43f6015e33c333fd35d
akanksha-1603 <akankshakolhe160@gmail.com>
2026-04-22 15:42:57 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "demo" {
   ami           = var.ami_id
   instance_type = var.instance_type
-
+   subnet_id = "subnet-01b4f47b536825517"
   tags = {
     Name = "Jenkins-Terraform-Demo"
   }
